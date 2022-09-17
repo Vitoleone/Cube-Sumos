6479e25792880ec2821e3678b0d72bf2d27b3c65
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-09-17 20:59:36 +0300

    CameraFollowing.cs added
    Now camera is following our player with dotween plugin

---
+++ Assets/Scripts/OtherObjectsScripts/CameraFollowing.cs
@@ -0,0 +1,23 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    public Tweener moveTween;
+    
+    
+
+    void Update()
+    {
+        
+        if(Player != null)
+        {
+            //moveTween = transform.DOMove(new Vector3(Player.transform.position.x,transform.position.y,Player.transform.position.z-5f), 0, false);//camera's position should be player's backside, for this we can do some adjustment on z axis.
+            transform.position = new Vector3(Player.transform.position.x, 8.689468f, Player.transform.position.z - 5f);
+        }
+        
+    }
+}
