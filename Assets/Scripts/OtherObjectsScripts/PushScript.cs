67b2b17481e0efa058304c968c1aea30789f9b92
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-09-18 10:08:28 +0300

    PushScript.cs added
    Push mechanic added.

---
+++ Assets/Scripts/OtherObjectsScripts/PushScript.cs
@@ -0,0 +1,56 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PushScript : MonoBehaviour
+{
+    Rigidbody myRb;
+    public float pushPower;
+    private void Awake()
+    {
+        myRb = gameObject.GetComponent<Rigidbody>();
+    }
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+        Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+
+            enemyRigidbody.AddRelativeForce(awayFromPlayer * pushPower*Time.deltaTime, ForceMode.Impulse);
+            collision.gameObject.GetComponent<EnemyMovement>().instance.isPushed = true;
+            StartCoroutine(WaitForSeconds(collision.gameObject));
+        }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            enemyRigidbody.AddRelativeForce(awayFromPlayer * pushPower * Time.deltaTime, ForceMode.Impulse);
+            collision.gameObject.GetComponent<PlayerMovement>().instance.isPushed = true;
+            StartCoroutine(WaitForSeconds(collision.gameObject));     
+        }
+    }
+    
+       
+    IEnumerator WaitForSeconds(GameObject collision)
+    {
+        
+        yield return new WaitForSeconds(0.5f);
+        
+       if(collision.CompareTag("Player"))
+       {
+            collision.GetComponent<PlayerMovement>().instance.isPushed = false;
+            
+       }
+       else if(collision.CompareTag("Enemy"))
+       {
+                collision.GetComponent<EnemyMovement>().instance.isPushed = false;
+       }
+        
+        
+    }
+ }
+   
+
