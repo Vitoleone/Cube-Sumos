e3bb92a85b4499ae8ef84544f8b401dce36f16f9
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-09-18 17:41:17 +0300

    FinishScreen.cs added
    Now if player dies or there are no sumos in ring, finishScreen will open.

---
+++ Assets/Scripts/UIScripts/FinishScreen.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinishScreen : MonoBehaviour
+{
+    [SerializeField] GameObject finishScreen;
+    [SerializeField] GameObject scoreText;
+    [SerializeField] GameObject sumosDefeatedText;
+    [SerializeField] GameObject positionText;
+    [SerializeField] GameObject gameManager;
+
+
+    private void Update()
+    {
+        if(gameManager.GetComponent<GameManager>().instance.isGameOver == true)
+        {
+            
+            finishScreen.SetActive(true);
+            scoreText.GetComponent<TextMeshProUGUI>().text = gameManager.GetComponent<GameManager>().instance.score.ToString();
+            positionText.GetComponent<TextMeshProUGUI>().text = "#" + gameManager.GetComponent<PositionScript>().instance.position.ToString();
+        }
+        
+    }
+
+}
