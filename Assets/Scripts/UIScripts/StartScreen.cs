d90d4acb7c82d9a716fe4c1791337e2e9ee03bbd
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-09-18 12:38:41 +0300

    Startscreen.cs added
    This script controlls the startscreen UI animation.

---
+++ Assets/Scripts/UIScripts/StartScreen.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using TMPro;
+
+public class StartScreen : MonoBehaviour
+{
+    [SerializeField] GameObject hand;
+    [SerializeField] GameObject TouchAndPlayText;
+    [SerializeField] GameObject CountDownText;
+    float countDownNumber = 4;
+    [SerializeField] GameObject startScreen;
+    
+    [SerializeField] GameManager gameManager;
+
+    void Start()
+    {
+        hand.transform.DOMoveX(276, 1f).SetLoops(10000, LoopType.Yoyo).SetEase(Ease.InOutFlash);
+        TouchAndPlayText.transform.DOScale(1.2f, 0.5f).SetLoops(10000, LoopType.Yoyo).SetEase(Ease.InOutFlash);
+        CountDownText.transform.DOScale(1.2f, 0.5f).SetLoops(10000, LoopType.Yoyo).SetEase(Ease.InOutFlash);
+    }
+    private void Update()
+    {
+        countDownNumber -= Time.deltaTime * 0.9f;
+        if (countDownNumber <= 4 && countDownNumber > 0)
+        {
+            
+            CountDownText.GetComponent<TextMeshProUGUI>().text = ((int)countDownNumber).ToString();
+        }
+        else
+        {
+            CountDownText.GetComponent<TextMeshProUGUI>().text = "GO";
+            if(countDownNumber < -0.5f && CountDownText.GetComponent<TextMeshProUGUI>().text == "GO")
+            {
+                StartGame();
+            }
+            
+        }
+        
+    }
+    public void StartGame()
+    {
+
+        gameManager.instance.isGameStarted = true;
+        startScreen.SetActive(false);
+        
+    }
+
+   
+}
